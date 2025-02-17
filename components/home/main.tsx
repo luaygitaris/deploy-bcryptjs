1eefb1b62e9d187c8aeaf967ea38f511a081e9ac
Luay Bachtiar <luaygitaris@gmail.com>
2025-02-17 20:36:16 +0700

    merubah file pada components home dari js ke tsx

--- components/home/main.tsx
+++ components/home/main.tsx
@@ -5,23 +5,29 @@ import Link from 'next/link';
 import { useState } from 'react';
 import { FaArrowAltCircleDown, FaArrowAltCircleRight } from 'react-icons/fa';
 
-export default function MainComponent() {
-  const [openIndex, setOpenIndex] = useState(null);
-  const [openContent, setOpenContent] = useState(null)
+interface Content {
+  id: number;
+  title: string;
+  isi: string;
+}
 
-  const toggleContent = (index) => {
+export default function MainComponent() {
+  const [openIndex, setOpenIndex] = useState<number | null>(null);
+  const [openContent, setOpenContent] = useState<number | null>(null);
+
+  const toggleContent = (index: number) => {
     setOpenIndex((prevIndex) => (prevIndex === index ? null : index));
   };
-  const toggleContentAsk = (index) => {
+
+  const toggleContentAsk = (index: number) => {
     setOpenContent((prevIndex) => (prevIndex === index ? null : index));
   };
-
 
   return (
     <div className="flex flex-wrap justify-between mt-10">
       <main className="p-10 w-full md:w-1/2">
         <h1 className="text-2xl font-bold">Fitur Unggulan:</h1>
-        {HeroContent.map((content) => (
+        {HeroContent.map((content: Content) => (
           <div key={content.id} className="mb-5">
             <div
               className="flex items-center gap-4 justify-between bg-ungu text-white p-5 rounded-md cursor-pointer"
@@ -45,8 +51,8 @@ export default function MainComponent() {
       </main>
 
       <main className="p-10 w-full md:w-1/2">
-      <h1 className="text-2xl font-bold">Kenapa memilih kami?</h1>
-        {HeroContentDua.map((content) => (
+        <h1 className="text-2xl font-bold">Kenapa memilih kami?</h1>
+        {HeroContentDua.map((content: Content) => (
           <div key={content.id} className="mb-5">
             <div
               className="flex items-center gap-4 justify-between bg-ungu text-white p-5 rounded-md cursor-pointer"
@@ -67,12 +73,18 @@ export default function MainComponent() {
             )}
           </div>
         ))}
-        <div className='border-2 flex flex-col justify-center items-center p-4'>
-            <h1 className='font-bold text-center md:text-left'>Dapatkan Pengalaman Manajemen Sekolah yang Lebih Baik!</h1>
-            <div className='flex'>
-               
-                <p className='text-center'><Link href={'/register'} className='text-blue-500 font-bold'>Daftar sekarang </Link>dan rasakan kemudahan dalam mengelola aktivitas sekolah dengan cara yang lebih efektif.</p>
-            </div>
+        <div className="border-2 flex flex-col justify-center items-center p-4">
+          <h1 className="font-bold text-center md:text-left">
+            Dapatkan Pengalaman Manajemen Sekolah yang Lebih Baik!
+          </h1>
+          <div className="flex">
+            <p className="text-center">
+              <Link href={'/register'} className="text-blue-500 font-bold">
+                Daftar sekarang{' '}
+              </Link>
+              dan rasakan kemudahan dalam mengelola aktivitas sekolah dengan cara yang lebih efektif.
+            </p>
+          </div>
         </div>
       </main>
     </div>
